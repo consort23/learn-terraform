d20bf8554b704f065628208496caf20846662a23
Ramesh Babu <consort23@gmail.com>
2024-03-23 19:07:49 +0530

    changes to terraform ec2 instnace for expense project

---
+++ expense-v1/ec2.tf
@@ -0,0 +1,43 @@
+
+resource "aws_instance" "frontend" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.security_groups
+  tags = {
+    Name = "frontend"
+  }
+}
+
+
+
+resource "aws_instance" "backend" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.security_groups
+  tags = {
+    Name = "backend"
+  }
+}
+
+
+
+resource "aws_instance" "mysql" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.security_groups
+  tags = {
+    Name = "mysql"
+  }
+}
+
+variable "ami" {
+  default = "ami-05f020f5935e52dc4"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "security_groups" {
+  default = ["sg-06b8bece9ed37b3dc"]
+}
